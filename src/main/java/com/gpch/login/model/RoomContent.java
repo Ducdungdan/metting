cbc1c4f806d4d2a4b5dbcc6693e927b3346450cb
Lê Văn Đức <levanduc10021996@gmail.com>
2018-10-23 01:45:29 +0700

    Add table entity User, Room, RoomUser, RoomRole, RoomUser, RoomCode, RoomContent, RoomContentReport, LogUser, LogUserCategory

---
+++ src/main/java/com/gpch/login/model/RoomContent.java
@@ -0,0 +1,134 @@
+package com.gpch.login.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.context.annotation.Primary;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "room_content")
+public class RoomContent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "room_content_id")
+    private int id;
+    
+    @Column(name = "name")
+    @Length(min = 10, message = "*Your name must have at least 10 characters")
+    @NotEmpty(message = "*Please provide name room role")
+    private String name;
+    
+    @Column(name = "content")
+    @Length(min = 20, message = "*Your name must have at least 20 characters")
+    @NotEmpty(message = "*Please provide content message")
+    private String content;
+    
+    @Column(name = "start")
+    private Timestamp start;
+    
+    @Column(name = "end")
+    private Timestamp end;
+    
+    @ManyToOne
+    @JoinColumn(name = "created_by", referencedColumnName="user_id")
+    private User user;
+    
+    @Column(name = "created_dtg")
+    private Timestamp createdDTG;
+    
+    @Column(name = "updated_by")
+    private int updatedBy;
+    
+    @Column(name = "updated_dtg")
+    private Timestamp updatedDTG;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Timestamp getStart() {
+		return start;
+	}
+
+	public void setStart(Timestamp start) {
+		this.start = start;
+	}
+
+	public Timestamp getEnd() {
+		return end;
+	}
+
+	public void setEnd(Timestamp end) {
+		this.end = end;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Timestamp getCreatedDTG() {
+		return createdDTG;
+	}
+
+	public void setCreatedDTG(Timestamp createdDTG) {
+		this.createdDTG = createdDTG;
+	}
+
+	public int getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(int updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Timestamp getUpdatedDTG() {
+		return updatedDTG;
+	}
+
+	public void setUpdatedDTG(Timestamp updatedDTG) {
+		this.updatedDTG = updatedDTG;
+	}
+    
+}
--- src/main/java/com/gpch/login/model/User.java
+++ src/main/java/com/gpch/login/model/User.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.Set;
 
 @Data
@@ -23,64 +27,167 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
     private int id;
-    @Column(name = "email")
-    @Email(message = "*Please provide a valid Email")
-    @NotEmpty(message = "*Please provide an email")
-    private String email;
+    
+    @Column(name = "username")
+    @Email(message = "*Please provide a valid username")
+    @NotEmpty(message = "*Please provide an username")
+    private String username;
+    
     @Column(name = "password")
     @Length(min = 5, message = "*Your password must have at least 5 characters")
     @NotEmpty(message = "*Please provide your password")
     private String password;
-    @Column(name = "name")
-    @NotEmpty(message = "*Please provide your name")
-    private String name;
+    
+    @Column(name = "first_name")
+    @NotEmpty(message = "*Please provide your first name")
+    private String firstName;
+    
     @Column(name = "last_name")
     @NotEmpty(message = "*Please provide your last name")
     private String lastName;
+    
+    @Column(name = "phone")
+    @Length(min = 10, max = 11, message = "*Your phone must have at least 10 characters and less 11 characters")
+    @NotEmpty(message = "*Please provide your phone")
+    private String phone;
+    
     @Column(name = "active")
     private int active;
+    
+    @Column(name = "created_by")
+    private int createdBy;
+    
+    @Column(name = "created_dtg")
+    private Timestamp createdDTG;
+    
+    @Column(name = "updated_by")
+    private int updatedBy;
+    
+    @Column(name = "updated_dtg")
+    private Timestamp updatedDTG;
+
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public int getActive() {
-		return active;
-	}
-	public void setActive(int active) {
-		this.active = active;
-	}
-	public Set<Role> getRoles() {
-		return roles;
-	}
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
-	}
+
+    @OneToMany(cascade=CascadeType.ALL)
+    @JoinColumn(name="user_id", referencedColumnName="created_by")
+    private Set<Room> ownRooms;
+    
+    @OneToMany(cascade=CascadeType.ALL)
+    @JoinColumn(name="user_id")
+    private Set<RoomUser> memberRooms;
+
 	public int getId() {
 		return id;
 	}
-    
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getActive() {
+		return active;
+	}
+
+	public void setActive(int active) {
+		this.active = active;
+	}
+
+	public int getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(int createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Timestamp getCreatedDTG() {
+		return createdDTG;
+	}
+
+	public void setCreatedDTG(Timestamp createdDTG) {
+		this.createdDTG = createdDTG;
+	}
+
+	public int getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(int updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Timestamp getUpdatedDTG() {
+		return updatedDTG;
+	}
+
+	public void setUpdatedDTG(Timestamp updatedDTG) {
+		this.updatedDTG = updatedDTG;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public Set<Room> getOwnRooms() {
+		return ownRooms;
+	}
+
+	public void setOwnRooms(Set<Room> ownRooms) {
+		this.ownRooms = ownRooms;
+	}
+
+	public Set<RoomUser> getMemberRooms() {
+		return memberRooms;
+	}
+
+	public void setMemberRooms(Set<RoomUser> memberRooms) {
+		this.memberRooms = memberRooms;
+	}
 
 }
