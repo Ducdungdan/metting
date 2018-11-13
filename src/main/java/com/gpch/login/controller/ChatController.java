70202720d013f6b2e2d3dec778ec8cb8c5a41496
Nguyễn Khắc Tư <nguyenkhactu1996@gmail.com>
2018-11-13 13:55:50 +0700

    done chat

---
+++ src/main/java/com/gpch/login/controller/ChatController.java
@@ -0,0 +1,36 @@
+package com.gpch.login.controller;
+
+import com.gpch.login.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class ChatController {
+
+    @RequestMapping(value = "/chat", method = RequestMethod.GET)
+    public String openChat(){
+        return "index";
+    }
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/hello")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/hello")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
