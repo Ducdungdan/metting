c54434fb31a2e84916ca0d6412cbd6ee4979c7b2
Home <Home@DESKTOP-2Q8A7SE>
2018-11-17 09:08:09 +0700

    commit

--- src/main/resources/static/js/meeting.js
+++ src/main/resources/static/js/meeting.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
 		removeActive();
 		$(this).addClass("active");
 		var lstChildren = $(this).children();
-		var userName = lstChildren[0].value;
+		var speakerID = lstChildren[0].value;
 		var fullName = lstChildren[1].value;
-		$("#hidUserName").val(userName);
+		$("#hidSpeakerID").val(speakerID);
 		$("#hidFullName").val(fullName);
 		$("#message").removeAttr('disabled');
-		$("#message").val("");
 		sendMessage();
+	
 		
 	});
 	var roomID = GetURLParameter("roomID");
@@ -93,73 +93,6 @@ sendMessage = function(){
 		var messageText = document.createTextNode(message);
 		textElement.appendChild(messageText);
 		messageElement.appendChild(textElement);
-		var messageArea = document.querySelector('.lstMessage');
-		messageArea.appendChild(messageElement);
-		messageArea.scrollTop = messageArea.scrollHeight;
-
-	}
-	
-	
-	
-}
-
-sendMessage1 = function(){
-	var message = "helelel";
-	$("#message").val("");
-	if (message.trim().length > 0) {
-		var fullName = "Nguyen Dinh Thang"
-		var lstFullNameSplit = fullName.split(" ");
-		var nameDisplay = "";
-		var lstMessage = $(".lstMessage");
-		for(var i=0; i< lstFullNameSplit.length -1 ; i++){
-			nameDisplay += lstFullNameSplit[i].charAt(0);
-		}
-		nameDisplay+="."+lstFullNameSplit[lstFullNameSplit.length - 1];
-		var currentDate = getActualFullDate();
-		var messageElement = document.createElement('li');
-		messageElement.classList.add('message');
-		var avatarElement = document.createElement('i');
-		var avatarText = document.createTextNode(lstFullNameSplit[lstFullNameSplit.length - 1][0]);
-		avatarElement.appendChild(avatarText);
-		avatarElement.style['background-color'] = getAvatarColor(fullName);
-		messageElement.appendChild(avatarElement);
-		var usernameElement = document.createElement('span');
-		var usernameText = document.createTextNode(nameDisplay);
-		usernameElement.appendChild(usernameText);
-		usernameElement.style['font-weight'] = 'bold';
-		messageElement.appendChild(usernameElement);
-		var timeElement= document.createElement('span');
-		var timeText = document.createTextNode(currentDate);
-		timeElement.appendChild(timeText);
-		timeElement.style['margin-left'] ='25px';
-		timeElement.style['font-style'] ='italic';
-		messageElement.appendChild(timeElement);
-		var textElement = document.createElement('textarea');
-		var messageText = document.createTextNode(message);
-		var icondelete = document.createElement('i');
-		var iconsave = document.createElement('i');
-		icondelete.setAttribute("class","fa fa-trash-o");
-		icondelete.style['color']='red';
-		icondelete.style['left']='unset';
-		icondelete.style['cursor']='-webkit-grab';
-		icondelete.setAttribute("aria-hidden","true");
-		icondelete.setAttribute("onclick","onclickDelelte()");
-		
-		iconsave.setAttribute("class","fa fa-floppy-o");
-		iconsave.style['color']='#4cae4c';
-		iconsave.style['left']='unset';
-		iconsave.style['cursor']='-webkit-grab';
-		iconsave.style['margin-left']='35px';
-		iconsave.setAttribute("aria-hidden","true");
-		iconsave.setAttribute("onclick","onclickSave()");
-		
-		textElement.appendChild(messageText);
-		textElement.setAttribute("class","form-control");
-		textElement.style['display']='inline-block';
-		textElement.style['width']='90%';
-		messageElement.appendChild(textElement);
-		messageElement.appendChild(icondelete);
-		messageElement.appendChild(iconsave);
 		var messageArea = document.querySelector('.lstMessage');
 		messageArea.appendChild(messageElement);
 		messageArea.scrollTop = messageArea.scrollHeight;
@@ -618,3 +551,97 @@ lstUserIDRemoved= [];
 		}
 	});
  }
+
+ // ------------------------------------------- SEND MESSAGE SOCKET -----------------------------------------------------
+
+var stompClient = null; 
+
+ // connect to socket
+ function connect(event) {
+
+        var socket = new SockJS('/ws');
+        stompClient = Stomp.over(socket);
+        stompClient.connect({}, onConnected, onError);
+        event.preventDefault();
+}
+
+
+function onConnected() {
+    // Subscribe to the Public Topic
+    var urlSocket = "/topic/"+GetURLParameter("roomID");
+    stompClient.subscribe(urlSocket, onMessageReceived);
+
+    // Tell your username to the server
+    var authorization = getCookiebyName("authorization");
+    stompClient.send("/app/chat.addUser",
+        {},
+        JSON.stringify({data: {"authorization": authorization}, type: 'JOIN'})
+    )
+}
+
+function onError(error) {
+    alert("Không thể kết nối với server, vui lòng refresh trang để thử lại");
+}
+
+function sendMessage(event) {
+    var speakerID = $("hidSpeakerID").val();
+    var roomID = GetURLParameter("roomID");
+    var content = $("#message").val().trim();
+    if(messageContent && stompClient) {
+        var chatMessage = {
+        		data: {
+        			roomId: roomID,
+        			speakerId: speakerID,
+        			content: content,
+        			startTime: new Date().getTime(),
+        			endTime: new Date().getTime() + 3600
+        		},
+            type: 'CHAT'
+        };
+
+        stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
+    }
+    event.preventDefault();
+}
+
+function onMessageReceived(payload) {
+	console.log(payload);
+    var message = JSON.parse(payload.body);
+
+    var messageElement = document.createElement('li');
+
+    if(message.type === 'JOIN') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' joined!';
+    } else if (message.type === 'LEAVE') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' left!';
+    } else if (message.type === 'CHAT') {
+    	messageElement.classList.add('chat-message');
+
+    	var reporter = message.data.reporter;
+    	var speaker = message.data.speaker
+    	
+        var avatarElement = document.createElement('i');
+        var avatarText = document.createTextNode(reporter.lastName[0]);
+        avatarElement.appendChild(avatarText);
+        avatarElement.style['background-color'] = getAvatarColor(reporter.firstName + " " + reporter.lastName);
+
+        messageElement.appendChild(avatarElement);
+
+        var usernameElement = document.createElement('span');
+        var usernameText = document.createTextNode(reporter.firstName + " " + reporter.lastName);
+        usernameElement.appendChild(usernameText);
+        messageElement.appendChild(usernameElement);
+    } 
+
+    var textElement = document.createElement('p');
+    var messageText = document.createTextNode(message.data.startTime + " " + message.data.endTime + " " + message.data.speaker.firstName + message.data.speaker.lastName + ": " + message.data.content);
+    textElement.appendChild(messageText);
+
+    messageElement.appendChild(textElement);
+
+    messageArea.appendChild(messageElement);
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
+
