d7be7e22f4f316b5b7503174da45f3174164b9e5
Nguyễn Khắc Tư <nguyenkhactu1996@gmail.com>
2018-11-29 22:53:33 +0700

    gen doc

--- src/main/java/com/gpch/login/utils/MergeFileExcelsUtil.java
+++ src/main/java/com/gpch/login/utils/MergeFileExcelsUtil.java
@@ -3,89 +3,89 @@ package com.gpch.login.utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Vector;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Component
 public class MergeFileExcelsUtil {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
     @Autowired
     ReadFileExcelUtil readFileExcelUtil;
     public List<Vector<String>> merge(int roomId){
         List<Vector<String>> result = new ArrayList<>();
-        List<Vector<String>> resultFinal = new ArrayList<>();
         List<Vector<String>> listWho = readFileExcelUtil.readWhoFile(roomId);
         List<Vector<String>> listWhat = readFileExcelUtil.readWhatFile(roomId);
+        if(listWhat.size() == 0 || listWho.size() == 0){
+            return new ArrayList<>();
+        }
         System.out.println("WHO: " + listWho.toString());
         System.out.println("WHAT: " + listWhat.toString());
-        for(Vector<String> what : listWhat){
-            for(Vector<String> who: listWho){
-                boolean timeStartValid = Integer.parseInt(what.get(0)) == Integer.parseInt(who.get(0));
-                boolean timeEndValid = Integer.parseInt(what.get(1)) == Integer.parseInt(who.get(1));
-                boolean fvalid = Integer.parseInt(what.get(3)) == Integer.parseInt(who.get(3));
-                if(timeEndValid && timeStartValid && fvalid){
-                    Vector<String> v = new Vector<>();
-                    v.add(what.get(0));
-                    v.add(what.get(1));
-                    v.add(who.get(2));
-                    v.add(what.get(2));
-                    v.add(what.get(3));
-                    if(result.size() == 0){
-                        result.add(v);
-                    }else{
-                        if(checkValid(v, result.get(result.size()-1))){
-                            result.add(v);
-                        }
-                    }
-                }
+
+        Map<String, List<String>> listTanSo = new HashMap<>();
+        for(Vector<String> what: listWhat){
+            if(listTanSo.containsKey(what.get(3))){
+                List<String> oldSpeakers = listTanSo.get(what.get(3));
+                List<String> newSpeakers = getSpeakerByF(what, listWho);
+                List<String> unionSpeaker = union(oldSpeakers, newSpeakers);
+                listTanSo.remove(what.get(3));
+                listTanSo.put(what.get(3), unionSpeaker);
+            }else{
+                listTanSo.put(what.get(3), getSpeakerByF(what, listWho));
             }
         }
-        System.out.println("RESULT" + result);
-        int flag = 0;
-        for(int i = 0; i< result.size(); i++){
-            if(i < flag) break;
-            int f = Integer.parseInt(result.get(i).get(4));
-            for(int j = i+1; j < result.size(); j++){
-                int ff = Integer.parseInt(result.get(j).get(4));
-                if(f == ff){
-                    String content = result.get(i).get(3);
-                    content += ".";
-                    content += result.get(j).get(3);
-                    result.get(i).set(i, content);
-                }else{
-                    Vector<String> v = new Vector<>();
-                    v.add(result.get(i).get(0));
-                    v.add(result.get(i).get(1));
-                    v.add(result.get(i).get(2));
-                    v.add(result.get(i).get(3));
-                    resultFinal.add(v);
-                    flag = j;
+
+        for(Vector<String> what: listWhat){
+            what.add(4, listTanSo.get(what.get(3)).get(0));
+        }
+        System.out.println(listWhat.toString());
+        return listWhat;
+    }
+
+    public List<String> getSpeakerByF(Vector<String> what, List<Vector<String>> listWho){
+        List<String> listSpeaker = new ArrayList<>();
+        long start_1 = parseDate(what.get(0));
+        long end_1 = parseDate(what.get(1));
+        for(Vector<String> who: listWho){
+            long start_2 = parseDate(who.get(0));
+            long end_2 = parseDate(who.get(1));
+            if(checkTime(start_1, end_1, start_2, end_2)){
+                listSpeaker.add(who.get(2));
+            }
+        }
+        return listSpeaker;
+    }
+
+
+    public long parseDate(String date){
+        Date d = null;
+        try {
+            d = simpleDateFormat.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return -1;
+        }
+        return d.getTime();
+    }
+
+    public boolean checkTime(long start_1, long end_1, long start_2, long end_2){
+        if(start_1 == start_2 && end_1 == end_2){
+            return true;
+        }
+        return false;
+    }
+
+    public List<String> union(List<String> l1, List<String> l2){
+        List<String> result = new ArrayList<>();
+        for(String e1: l1){
+            for(String e2: l2){
+                if(e1.endsWith(e2)){
+                    result.add(e1);
                     break;
                 }
             }
-            if(i == result.size()-1){
-                Vector<String> v = new Vector<>();
-                v.add(result.get(i).get(0));
-                v.add(result.get(i).get(1));
-                v.add(result.get(i).get(2));
-                v.add(result.get(i).get(3));
-                resultFinal.add(v);
-            }
         }
-        return resultFinal;
-    }
-
-    public boolean checkValid(Vector<String> v1, Vector<String> v2){
-        int count = 0;
-        for(int i = 0; i< v1.size(); i++){
-            if(v1.get(i).equals(v2.get(i))){
-                count++;
-            }
-        }
-        if(count == v1.size()){
-            return false;
-        }
-        return true;
+        return result;
     }
 
 }
